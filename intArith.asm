346a5a766a0c027c7a18f55e312e853d756b4e0b
Ray Allan <fierlion@gmail.com>
2014-03-13 10:24:15 -0700

    Update intArith.asm

--- intArith.asm
+++ intArith.asm
@@ -9,25 +9,25 @@ INCLUDE Irvine32.inc
 
 .data
 my_name			BYTE			"           Elementary Arithmetic       by Ray Foote. ", 0
-user_instruct1	BYTE			"Enter two integers.  This program will display their sum, diff,", 0
-user_instruct2	BYTE			"prod, and quot (and its remainder).", 0
+user_instruct1		BYTE			"Enter two integers.  This program will display their sum, diff,", 0
+user_instruct2		BYTE			"prod, and quot (and its remainder).", 0
 prompt1			BYTE			"First integer: ", 0
 prompt2			BYTE			"Second integer: ", 0
-user_int1			DWORD		?
-user_int2			DWORD		?
-sum_res			DWORD		?
-diff_res			DWORD		?
-prod_res			DWORD		?
-quot_res			DWORD		?
-remain				DWORD		?
-plus				BYTE			' + ',0
-minus				BYTE			' - ',0
-times				BYTE			' * ',0
-divide				BYTE			' / ',0
-equals				BYTE			' = ',0
-remainder			BYTE			" remainder ", 0
+user_int1		DWORD			?
+user_int2		DWORD			?
+sum_res			DWORD			?
+diff_res		DWORD			?
+prod_res		DWORD			?
+quot_res		DWORD			?
+remain			DWORD			?
+plus			BYTE			' + ',0
+minus			BYTE			' - ',0
+times			BYTE			' * ',0
+divide			BYTE			' / ',0
+equals			BYTE			' = ',0
+remainder		BYTE			" remainder ", 0
 goodbye			BYTE			"The End.  Goodbye.", 0
-again_ask			BYTE			"Want to do another? ",0
+again_ask		BYTE			"Want to do another? ",0
 ag_prompt		BYTE			"Enter '1' for 'yes', '0' to end: ",0
 
 
@@ -35,144 +35,137 @@ ag_prompt		BYTE			"Enter '1' for 'yes', '0' to end: ",0
 main PROC
 
 ; Introduction
-	mov	edx, OFFSET my_name
+	mov		edx, OFFSET my_name
 	call		WriteString
 	call		CrLf
 	call		CrLf
 L1:
 ; color change
 	call		Randomize
-	mov	eax, 14										; color between black(0) and lightMagenta(13)
+	mov		eax, 14						; color between black(0) and lightMagenta(13)
 	call		RandomRange
-	mov	ebx, 16
-	mul	ebx
-	add	eax, 15
+	mov		ebx, 16
+	mul		ebx
+	add		eax, 15
 	call		SetTextColor
 
 ; prompt for user input
-	mov	edx, OFFSET user_instruct1
+	mov		edx, OFFSET user_instruct1
 	call		WriteString
 	call		CrLf
-	mov	edx, OFFSET user_instruct2
+	mov		edx, OFFSET user_instruct2
 	call		WriteString
 	call		CrLf
 	call		CrLf
 
 ; Get user data
-	mov	edx, OFFSET prompt1
+	mov		edx, OFFSET prompt1
 	call		WriteString
-     call		ReadInt
-     mov	user_int1, eax
+     	call		ReadInt
+     	mov		user_int1, eax
 	call		CrLf
-	mov	edx, OFFSET prompt2
+	mov		edx, OFFSET prompt2
 	call		WriteString
 	call		ReadInt
-	mov	user_int2, eax
+	mov		user_int2, eax
 	call		CrLf
 
 ; Calculate required values
 
-	mov	eax, user_int1								; add
-	mov	ebx, user_int2
-	add	eax, ebx
-	mov	sum_res, eax
+	mov		eax, user_int1					; add
+	mov		ebx, user_int2
+	add		eax, ebx
+	mov		sum_res, eax
 
-	mov	eax, user_int1								; add
-	mov	ebx, user_int2
-	sub	eax, ebx
-	mov	diff_res, eax
+	mov		eax, user_int1					; subtract
+	mov		ebx, user_int2
+	sub		eax, ebx
+	mov		diff_res, eax
 	
-	mov	eax, user_int1								; multiply
-	mov	ebx, user_int2
-	mul	ebx
-	mov	prod_res, eax
+	mov		eax, user_int1					; multiply
+	mov		ebx, user_int2
+	mul		ebx
+	mov		prod_res, eax
 
-	mov	eax, user_int1								; divide
+	mov		eax, user_int1					; divide
 	cdq
-	mov	ebx, user_int2
+	mov		ebx, user_int2
 	div		ebx
-	mov	quot_res, eax
-	mov	remain, edx
+	mov		quot_res, eax
+	mov		remain, edx
 
 ; Display results
 	
-	mov	eax, user_int1								; plus result
+	mov		eax, user_int1					; plus result
 	call		WriteDec
-	mov	edx, OFFSET plus
+	mov		edx, OFFSET plus
 	call		WriteString
-	mov	eax, user_Int2
+	mov		eax, user_Int2
 	call		WriteDec
-	mov	edx, OFFSET equals
+	mov		edx, OFFSET equals
 	call		WriteString
-	mov	eax, sum_res
+	mov		eax, sum_res
 	call		WriteDec
 	call		CrLf
 
-	mov	eax, user_int1								; minus result
+	mov		eax, user_int1					; minus result
 	call		WriteDec
-	mov	edx, OFFSET minus
+	mov		edx, OFFSET minus
 	call		WriteString
-	mov	eax, user_Int2
+	mov		eax, user_Int2
 	call		WriteDec
-	mov	edx, OFFSET equals
+	mov		edx, OFFSET equals
 	call		WriteString
-	mov	eax, diff_res
+	mov		eax, diff_res
 	call		WriteDec
 	call		CrLf
 
-	mov	eax, user_int1								; multiply result
+	mov		eax, user_int1					; multiply result
 	call		WriteDec
-	mov	edx, OFFSET times
+	mov		edx, OFFSET times
 	call		WriteString
-	mov	eax, user_Int2
+	mov		eax, user_Int2
 	call		WriteDec
-	mov	edx, OFFSET equals
+	mov		edx, OFFSET equals
 	call		WriteString
-	mov	eax, prod_res
+	mov		eax, prod_res
 	call		WriteDec
 	call		CrLf
 
-	mov	eax, user_int1								; divide result
+	mov		eax, user_int1					; divide result
 	call		WriteDec
-	mov	edx, OFFSET divide
+	mov		edx, OFFSET divide
 	call		WriteString
-	mov	eax, user_Int2
+	mov		eax, user_Int2
 	call		WriteDec
-	mov	edx, OFFSET equals
+	mov		edx, OFFSET equals
 	call		WriteString
-	mov	eax, quot_res
+	mov		eax, quot_res
 	call		WriteDec
-	mov	edx, OFFSET remainder
+	mov		edx, OFFSET remainder
 	call		WriteString
-	mov	eax, remain
+	mov		eax, remain
 	call		WriteDec
 	call		CrLf
 	call		CrLf
 
 ; ask for repeat
-	mov	edx, OFFSET again_ask
+	mov		edx, OFFSET again_ask
 	call		WriteString
 	call		CrLf
-	mov	edx, OFFSET ag_prompt
+	mov		edx, OFFSET ag_prompt
 	call		WriteString
 	call		ReadInt
-	cmp	eax, 1
+	cmp		eax, 1
 	jge		L1
 
 ; Say goodbye if 'no'
-	mov	edx, OFFSET goodbye
+	mov		edx, OFFSET goodbye
 	call		WriteString
 	call		CrLf
 
 	exit	; exit to operating system
 main ENDP
 
-; (insert additional procedures here)
 
 END main
-
-
-		mov		eax, RAND_HI
-		inc			eax
-		mov		ebx, RAND_LOW
-		sub		eax, ebx		
